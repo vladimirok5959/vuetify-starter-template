a573ac4c108162d2dc4383bbafb617222e9908f7
Volodymyr Tkach <vladimirok5959@gmail.com>
2023-03-11 03:22:44 +0200

    Remove unused code

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -18,16 +18,5 @@
 </template>
 
 <script>
-  export default {
-    components: {
-    },
-    data () {
-      return {
-      }
-    },
-    methods: {
-    },
-    mounted() {
-    },
-  }
+  //
 </script>
