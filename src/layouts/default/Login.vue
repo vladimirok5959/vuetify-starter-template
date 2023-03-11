d20745e73f209ba336037c5651c29c3330a775d9
Volodymyr Tkach <vladimirok5959@gmail.com>
2023-03-11 02:52:37 +0200

    Move form to correct file

--- src/layouts/default/Login.vue
+++ src/layouts/default/Login.vue
@@ -1,50 +1,17 @@
 <template>
   <v-app id="inspire">
-    <v-container class="fill-height">
-      <v-responsive class="d-flex align-center fill-height">
-        <v-row>
-          <v-col cols="12" sm="8" offset-sm="2" md="6" offset-md="3">
-            <v-card>
-              <v-card-title>
-                <span class="text-h5">Please Login</span>
-              </v-card-title>
-              <v-card-text>
-                <v-container class="pa-0">
-                  <v-form v-model="valid">
-                    <v-text-field
-                      v-model="email"
-                      :rules="emailRules"
-                      label="E-mail*"
-                      type="text"
-                      required
-                    ></v-text-field>
-                    <v-text-field
-                      v-model="password"
-                      :rules="passwordRules"
-                      label="Password*"
-                      type="password"
-                      required
-                    ></v-text-field>
-                  </v-form>
-                </v-container>
-                <small>*indicates required field</small>
-              </v-card-text>
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn color="primary" variant="text" to="/">Login</v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-col>
-        </v-row>
-      </v-responsive>
-    </v-container>
+    <default-view />
   </v-app>
 </template>
 
 <script>
   import { useTheme } from 'vuetify'
+  import DefaultView from './View.vue'
 
   export default {
+    components: {
+      DefaultView,
+    },
     setup() {
       const theme = useTheme()
 
@@ -55,33 +22,6 @@
     },
     data() {
       return {
-        valid: false,
-        email: '',
-        emailRules: [
-          value => {
-            if(value) return true
-            return 'E-mail is requred'
-          },
-          value => {
-            if(/.+@.+\..+/.test(value)) return true
-            return 'E-mail must be valid'
-          },
-        ],
-        password: '',
-        passwordRules: [
-          value => {
-            if(value) return true
-            return 'Password is requred'
-          },
-          value => {
-            if(value?.length >= 8) return true
-            return 'Password must be more than 8 characters'
-          },
-          value => {
-            if(value?.length <= 32) return true
-            return 'Password must be less than 32 characters'
-          },
-        ],
       }
     },
   }
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,76 @@
 <template>
+  <v-container class="fill-height">
+    <v-responsive class="d-flex align-center fill-height">
+      <v-row>
+        <v-col cols="12" sm="8" offset-sm="2" md="6" offset-md="3">
+          <v-card>
+            <v-card-title>
+              <span class="text-h5">Please Login</span>
+            </v-card-title>
+            <v-card-text>
+              <v-container class="pa-0">
+                <v-form v-model="valid">
+                  <v-text-field
+                    v-model="email"
+                    :rules="emailRules"
+                    label="E-mail*"
+                    type="text"
+                    required
+                  ></v-text-field>
+                  <v-text-field
+                    v-model="password"
+                    :rules="passwordRules"
+                    label="Password*"
+                    type="password"
+                    required
+                  ></v-text-field>
+                </v-form>
+              </v-container>
+              <small>*indicates required field</small>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="primary" variant="text" to="/">Login</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-responsive>
+  </v-container>
 </template>
 
-<script setup>
-  //
+<script>
+  export default {
+    data() {
+      return {
+        valid: false,
+        email: '',
+        emailRules: [
+          value => {
+            if(value) return true
+            return 'E-mail is requred'
+          },
+          value => {
+            if(/.+@.+\..+/.test(value)) return true
+            return 'E-mail must be valid'
+          },
+        ],
+        password: '',
+        passwordRules: [
+          value => {
+            if(value) return true
+            return 'Password is requred'
+          },
+          value => {
+            if(value?.length >= 8) return true
+            return 'Password must be more than 8 characters'
+          },
+          value => {
+            if(value?.length <= 32) return true
+            return 'Password must be less than 32 characters'
+          },
+        ],
+      }
+    },
+  }
 </script>
